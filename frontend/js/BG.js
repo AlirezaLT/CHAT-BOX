d3e03792faee56ff105e97b2da9e487571256231
AlirezaLT <ali.ghost.anvari.lt@gmail.com>
2026-02-06 06:58:32 +0330

    BG BREATHING

--- frontend/js/BG.js
+++ frontend/js/BG.js
@@ -30,11 +30,32 @@ for (let i= 0; i < ORB_COUNT; i++) {
         speed: 0.003 + Math.random() * 0.002 });
     }
 
+    const mouse = {
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2
+    };
+
+    window.addEventListener("mousemove", (e) => {
+        mouse.x = e.clientX;
+        mouse.y = e.clientY;
+    });
+
     function animate(){
         orbs.forEach(o => {
         o.x += (o.tx - o.x) * o.speed;
         o.y += (o.ty - o.y) * o.speed;
-        
+
+        const dx = o.x - mouse.x;
+        const dy = o.y - mouse.y;
+
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        const influence = Math.max(0, 1 - distance / 400);
+            const force = influence * influence;
+
+        o.x += dx * force * 0.12;
+        o.y += dy * force * 0.12;
+
         o.el.style.transform =
     `translate(${o.x}px, ${o.y}px)`;
     if (Math.abs(o.x - o.tx) < 1 && Math.abs(o.y - o.ty) < 1){
